16b34732fe8a285c178873f6105986ea728369d0
unknown <quiensoyanna99@gmail.com>
2023-02-21 10:17:56 -0500

    Add changes in folders

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <main className="main">
+      <section>
+        <p>so, you want to travel to</p>
+        <h1>space</h1>
+        <p>
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../components/home/Home.jsx";
+import App from "../components/App.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   );
