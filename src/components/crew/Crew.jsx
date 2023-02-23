623342a3bb2496951e58db14d6f15cd827464cf8
dpaeza <danipaez1098@gmail.com>
2023-02-22 23:30:19 -0500

    create crew section version desktop 1.0

--- src/components/crew/Crew.jsx
+++ src/components/crew/Crew.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
+import { NavLink, useParams } from "react-router-dom";
+import axios from 'axios';
+import oval from "./../../assets/OvalCopy.svg"
+
+const URL_CREW = "https://mini-back-workshop-4-production.up.railway.app/crew";
 
 const Crew = () => {
-  return <div>Crew</div>;
+
+  const { crewId } = useParams();
+  const [crew, setCrew] = useState(null);
+
+  useEffect(()=>{
+    if(crewId){
+      axios.get(`${URL_CREW}?name_like=${crewId}`).then((response) => {
+        setCrew(response.data[0]);
+      });
+    }
+  }, [crewId])
+
+  return (
+    <section className="crew">
+      <section className="crew__main">
+        <div className="crew__main__div1">
+          <h3> <span>02</span> MEET YOUR CREW</h3>
+          <h2>{crew ? crew.role.toUpperCase() : ""}</h2>
+          <h1>{crew ? crew.name.toUpperCase() : ""}</h1>
+          <p>{crew ? crew.bio : ""}</p>
+          <nav>
+            <NavLink to="/crew/Douglas%20Hurley"><img src={oval} alt="oval icon" className={({ isActive }) => isActive ? 'active' : ''} /></NavLink>
+            <NavLink to="/crew/Mark%20Shuttleworth"><img src={oval} alt="oval icon" /></NavLink>
+            <NavLink to="/crew/Victor%20Glover"><img src={oval} alt="oval icon" /></NavLink>
+            <NavLink to="/crew/Anousheh%20Ansari"><img src={oval} alt="oval icon" /></NavLink>
+          </nav>
+        </div>
+        <div>
+          <figure><img src={crew ? crew.images.png : ""} alt="crew picture" className="crew__picture"/></figure>
+        </div>
+      </section>
+    </section>
+  )
 };
 
 export default Crew;
--- src/router/Router.js
+++ src/router/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../components/App.jsx";
 import Destination from '../components/destination/Destination';
+import Crew from "../components/crew/Crew.jsx";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Destination/:destinationId" element={<Destination />} />
+        <Route path="/Crew/:crewId" element={<Crew />} />
       </Routes>
     </BrowserRouter>
   );
